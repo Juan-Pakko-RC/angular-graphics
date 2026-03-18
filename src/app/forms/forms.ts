c64ccd24e507b43f59cefb40c8a4f36460d8dc7e
Juan Pablo R <juancaicedo1997@gmail.com>
2026-03-18 18:56:27 -0500

    Added a form and functionality to show user attributes

--- src/app/forms/forms.ts
+++ src/app/forms/forms.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { email } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-forms',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './forms.html',
   styleUrl: './forms.css',
 })
-export class Forms {}
+export class Forms {
+
+  public user: any = {
+    name: "",
+    email: ""
+  }
+
+  send(){
+    console.log(this.user);
+    
+  }
+
+}
